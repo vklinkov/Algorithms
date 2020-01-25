299f6cbda296045f8efadff9eb13aa441f225662
Viktor <vklinkov@ukr.net>
2020-01-25 20:44:17 +0200

    Initial commit

---
+++ Bro/Extentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bro
+{
+    public class Extentions
+    {
+        public bool Swap(string str)
+        {
+            if (str.Length > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class FooBaz
+    {
+
+    }
+}

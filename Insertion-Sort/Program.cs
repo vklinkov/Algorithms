66d679bd152240d7098f6d6de42efb0b5d3442f9
vklinkov <vklinkov@ukr.net>
2020-01-25 21:03:47 +0200

    Insertion-Sort

---
+++ Insertion-Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

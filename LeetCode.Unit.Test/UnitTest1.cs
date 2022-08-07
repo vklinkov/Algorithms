c107f8b8d15f766d3e584c8cd6a0dc3a5559b1bd
Viktor <vklinkov@ukr.net>
2022-08-07 13:36:49 +0300

    add test

--- LeetCode.Unit.Test/UnitTest1.cs
+++ LeetCode.Unit.Test/UnitTest1.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 
 namespace LeetCode.Unit.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void FooBazTest()
         {
+            FooBaz fooBaz = new FooBaz();
+            var res = fooBaz.GetFooBaz(5);
 
+            Assert.Equal(string.Empty, res);
         }
     }
 }

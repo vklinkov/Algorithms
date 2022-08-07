96f874dd84cc8a558545268745e5c28bf28b547f
Viktor <vklinkov@ukr.net>
2022-08-07 11:41:09 +0300

    add FooBaz implementation

---
+++ LeetCode/FooBaz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class FooBaz
+    {
+        public string GetFooBaz(int value)
+        {
+            if(value % 5 == 0)
+            {
+                return "Foo";
+            }
+            else if (value % 5 > 0)
+            {
+                return "Baz";
+            }
+            else
+            {
+                return "FooBaz";
+            }
+        }
+    }
+}
